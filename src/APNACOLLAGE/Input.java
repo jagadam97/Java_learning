7e578e45ff42bc754d7589b12e8160de00adde4f
Jagadam Dinesh Reddy <dineshjagadam@gmail.com>
2022-09-14 19:57:22 +0530

    new file:   src/APNACOLLAGE/Input.java

---
+++ src/APNACOLLAGE/Input.java
@@ -0,0 +1,20 @@
+package APNACOLLAGE;
+
+import java.util.*;
+
+public class Input {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        //nextline
+        String name = in.nextLine();
+        System.out.println(name);
+        //nextint
+        //nextfloat
+        //nextchar
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int sum = a + b;
+        in.close();
+        System.out.println(sum);
+    }
+}
