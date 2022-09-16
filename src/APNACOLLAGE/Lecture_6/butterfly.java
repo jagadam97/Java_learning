39ccde556e9f5d3c1822a20a2bc2babed178785b
dineshjagadam@gmail.com <dineshjagadam@gmail.com>
2022-09-16 21:33:32 +0530

    added butterfly

--- src/APNACOLLAGE/Lecture_6/butterfly.java
+++ src/APNACOLLAGE/Lecture_6/butterfly.java
@@ -2,7 +2,20 @@ public class butterfly{
 public static void main (String[] args) {
   int n = 4;
   for (int i = 1; i<n ; i++){
+    for (int j = 1; j<= i; j++){
+      System.out.print("*");
+    }
     
+    int space = 2*(n-1);
+    for(int j= 1 l; j<= space; j++){
+      System.out.print(" ");
+    }
+    
+    for (int j= 1 ; j<=i; j++){
+      System.oit.print("*");
+    }
+    
+    System.out.println();
   }
 }
 }
