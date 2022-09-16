dac373b81bab0cf935384cdaa74ed52e9855e999
Jagadam Dinesh Reddy <dineshjagadam@gmail.com>
2022-09-16 21:36:22 +0530

    modified:   src/APNACOLLAGE/Lecture_6/butterfly.java

--- src/APNACOLLAGE/Lecture_6/butterfly.java
+++ src/APNACOLLAGE/Lecture_6/butterfly.java
@@ -7,12 +7,12 @@ public static void main (String[] args) {
     }
     
     int space = 2*(n-1);
-    for(int j= 1 l; j<= space; j++){
+    for(int j= 1 ; j<= space; j++){
       System.out.print(" ");
     }
     
     for (int j= 1 ; j<=i; j++){
-      System.oit.print("*");
+      System.out.print("*");
     }
     
     System.out.println();
