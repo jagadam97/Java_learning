3819b3bb5b8e9d710e63c0e982e6a074dc4d8658
jagadam97 <dineshjagadam@gmail.com>
2022-11-21 15:36:46 +0530

    new file:   src/HackerRank/Introduction/welcomeToJava.java

---
+++ src/HackerRank/Introduction/welcomeToJava.java
@@ -0,0 +1,10 @@
+package HackerRank.Introduction;
+
+public class welcomeToJava 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java."); 
+    }
+}
